e74676d1622780f5786ca8572cd03ceb1b53d0d8
Magdalena Podolewska <magdalena@podolewska.com>
2021-11-01 16:07:06 +0100

    write.py implemented

--- write.py
+++ write.py
@@ -24,8 +24,14 @@ def write_to_csv(results, filename):
     :param results: An iterable of `CloseApproach` objects.
     :param filename: A Path-like object pointing to where the data should be saved.
     """
-    fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km', 'potentially_hazardous')
+    fieldnames = ('datetime_utc', 'distance_au', 'velocity_km_s', 'designation', 'name', 'diameter_km',
+                  'potentially_hazardous')
     # TODO: Write the results to a CSV file, following the specification in the instructions.
+    with open(filename, 'w') as outfile:
+        writer = csv.DictWriter(outfile, fieldnames)
+        writer.writeheader()
+        for row in results:
+            writer.writerow(row)
 
 
 def write_to_json(results, filename):
@@ -40,3 +46,5 @@ def write_to_json(results, filename):
     :param filename: A Path-like object pointing to where the data should be saved.
     """
     # TODO: Write the results to a JSON file, following the specification in the instructions.
+    with open(filename, 'w') as outfile:
+        json.dump(results, outfile)
